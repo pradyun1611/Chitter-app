bb72093aa25b05ea428e0cac6fd6c7854ec23e38
Pradyun Mondal <pradyun2.1611@gmail.com>
2025-06-21 21:20:10 +0530

    deleted:    backend/public/pfp/684c1d1696674f631c3e2890_1750060093072.jpeg 	new file:   backend/public/pfp/684c1d1696674f631c3e2890_1750264947771.png 	new file:   frontend/public/github-mark/github-mark-white.png 	new file:   frontend/public/github-mark/github-mark-white.svg 	new file:   frontend/public/github-mark/github-mark.png 	new file:   frontend/public/github-mark/github-mark.svg 	modified:   frontend/src/components/Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -170,7 +170,11 @@ function Layout({children, activeTab}) {
                             )}
                         </button>
                     </div>
-                    <div className='m-2'><a target='blank' href='https://github.com/pradyun1611'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" /></svg></a></div>
+                    <div className='m-2'><a target='blank' href='https://github.com/pradyun1611'>{darkMode ? (
+                        <img className='h-7 w-7' src='/github-mark/github-mark-white.svg' />
+                    ) : (
+                        <img className='h-7 w-7' src='/github-mark/github-mark.svg'/>
+                    )}</a></div>
                     {user ? (
                         <>
                         <Link className='h-full m-2 flex items-center justify-center' to={`/user/${curUser._id}`}><img src={`http://localhost:4000/pfp/${curUser.pfp}`} alt='pfp' className='h-7 w-7 object-cover rounded-full'/></Link>
